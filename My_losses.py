811588af6ad6af332227f2d2fd5449348fb41689
Mahmood_Haithami <mss3331@hotmail.com>
2022-04-08 15:22:02 +0800

    - Sobel Kernel is normalized

--- My_losses.py
+++ My_losses.py
@@ -12,13 +12,13 @@ def gradMaskLoss_Eq1(images,mask,loss_fn):
 
 def image_gradient(images,reduction='mean'):
     device = torch.device('cuda:0')
-    a = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    a = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])/4
     conv1 = nn.Conv2d(1, 1, kernel_size=3, stride=1, padding=1, bias=False)
     conv1.weight = nn.Parameter(torch.from_numpy(a).float().unsqueeze(0).unsqueeze(0), requires_grad=False)
     # print(conv1.weight)
     conv1.to(device)
     # -----------------------------------------------------------
-    b = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    b = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])/4
     conv2 = nn.Conv2d(1, 1, kernel_size=3, stride=1, padding=1, bias=False)
     conv2.weight = nn.Parameter(torch.from_numpy(b).float().unsqueeze(0).unsqueeze(0), requires_grad=False)
     conv2.to(device)
