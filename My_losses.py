- I fixed trivial but nasty bug in my Training.py loss calculation

My_losses.py
@@ -6,10 +6,8 @@ def gradMaskLoss_Eq1(images,mask,loss_fn):
     r'''‖∇g∙mask(g)‖^2 == MSELoss(∇g∙mask(g),torch.zeros(∇g.shape)'''
     device=torch.device('cuda:0')
     images_grad=image_gradient(images, reduction=None)
-    print(mask.shape,images_grad.shape)
-    exit(0)
     masked_grad = torch.mul(images_grad,1-mask)
-    return loss_fn(masked_grad,torch.zeros(masked_grad.shape).to(device))
+    return loss_fn(masked_grad,torch.zeros(images.shape).to(device))
 
 
 def image_gradient(images,reduction='mean'):

Training.py
@@ -43,7 +43,7 @@ def training_loop(n_epochs, optimizer, lamda, model, loss_fn, data_loader_dic, d
 
                 with torch.set_grad_enabled(phase == 'train'):
                     loss_l2 = loss_fn(ypred, X)*lamda["l2"]
-                    loss_grad = gradMaskLoss_Eq1(X,intermediate,loss_fn)*lamda["grad"]
+                    loss_grad = gradMaskLoss_Eq1(ypred,intermediate,loss_fn)*lamda["grad"]
 
                     loss = loss_grad+loss_l2
 