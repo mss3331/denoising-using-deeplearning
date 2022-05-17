c53265241dcf47b948731f9b5bb76b354347d5b1
Mahmood_Haithami <mss3331@hotmail.com>
2022-05-18 00:22:55 +0800

    - trying to develop inference code. still I didn't finish it

---
+++ DL_TOV_threeStages_inference.py
@@ -0,0 +1,149 @@
+#9064466c4a4f16db52c1672e03ee3c52060a24e4 token
+import torch.optim as optim
+# import matplotlib.pyplot as plt
+import wandb
+import random
+import time
+import os
+import numpy as np
+from Plotting import plot, plot_test
+from torch.nn import functional as F
+from MyDataloaders import *
+from Metrics import *
+from models import MyModelV1, FCNModels, DeepLabModels, unet
+import torch
+from MyDataloaders_denoising import getDataloadersDic
+from torch import nn
+from Training import *
+from torchvision import datasets
+from torch.utils.data import ConcatDataset
+from torch.utils.data import DataLoader
+
+
+
+wandb.login(key="38818beaffe50c5403d3f87b288d36d1b38372f8")
+# from prettytable import PrettyTable
+def initializWandb():
+    wandbproject_name = "denoising"
+    wandb.init(
+        project=wandbproject_name,
+        entity="mss3331",
+        name=experiment_name,
+        # Track hyperparameters and run metadata
+        config={
+            "learning_rate": learning_rate,
+            "optimizer": "Adam",
+            "architecture": model_name,
+            "batch_size": batch_size,
+            "lamda": lamda,
+            "num_epochs": num_epochs,
+            "dataset": root_dir.split("/")[-1], })
+
+def repreducibility():
+    torch.manual_seed(0)
+    np.random.seed(0)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+if __name__ == '__main__':
+    '''This main is created to do side experiments'''
+    repreducibility()
+    experiment_name='Denoising_DLTOV_ThreeStagestraining_Exp1_testingOnTestSet'
+    learning_rate = 0.01
+    input_channels = 3
+    number_classes = 3  # output channels should be one mask for binary class
+    switch_epoch = 50 # when to switch to the next training stage?
+    run_in_colab = True
+    root_dir = r"E:\Databases\dummyDataset\train"
+    child_dir = "data_C1"
+    imageDir = 'images_C1'
+    maskDir = 'mask_C1'
+    colab_dir = "."
+    if run_in_colab:
+        root_dir = "/content/CVC-ClinicDB"
+        colab_dir = "/content/denoising-using-deeplearning"
+    num_epochs = 1
+    batch_size = 7
+    shuffle = False
+    lamda = {"l2":20,"grad":20} #L2 and Grad
+
+    # ************** modify for full experiment *************
+    # load_to_RAM = True
+
+    resize_factor = 0.75
+    target_img_size = (int(288*resize_factor), int(384*resize_factor))
+    train_val_ratio = 0.5
+
+    print("resize_factor={} and image size={}".format(resize_factor, target_img_size))
+    # ************** modify for full experiment *************
+    # [SegNet, SegNetGRU, SegNetGRU_Symmetric, SegNetGRU_Symmetric_columns,
+    # SegNetGRU_Symmetric_columns_shared_EncDec, SegNetGRU_Symmetric_columns_UltimateShare,
+    # SegNetGRU_Symmetric_columns_last2stages, SegNetGRU_Symmetric_columns_last2stages_Notshared_EncDec
+    # SegNetGRU_Symmetric_columns_last2stages_Notshared_EncDec_smallerH, SegNetGRU_5thStage_only_not_shared,
+    # SegNetGRU_4thStage_only_not_shared, SegNetGRU_Symmetric_last2stages_FromEncToDec]
+    ########################### Deeplab versions ###################################
+    # [Deeplap_resnet50, Deeplap_resnet101, FCN_resnet50, FCN_resnet101, Deeplabv3_GRU_ASPP_resnet50,
+    # Deeplabv3_GRU_CombineChannels_resnet50, Deeplabv3_GRU_ASPP_CombineChannels_resnet50, Deeplabv3_LSTM_resnet50]
+    ########################### unet model #####################################################
+    # [unit.UNET]
+    model_name = "unet"
+    generator = unet.UNet(in_channels=input_channels,
+                      out_channels=number_classes,
+                      n_blocks=4,
+                      activation='relu',
+                      normalization='batch',
+                      conv_mode='same',
+                      dim=2)
+    generator = nn.Sequential(generator, nn.Sigmoid())
+    segmentor = unet.UNet(in_channels=input_channels,
+                      out_channels=2,
+                      n_blocks=4,
+                      activation='relu',
+                      normalization='batch',
+                      conv_mode='same',
+                      dim=2)
+    model = nn.ModuleList([generator, segmentor])
+    # Start WandB recording
+    initializWandb()
+    print("epochs {} batch size {}".format(num_epochs, batch_size))
+
+    dataset_info = [(root_dir, child_dir, imageDir, maskDir, target_img_size)]#,
+                    #("/content/trainData_EndoCV2021_5_Feb2021","data_C2","images_C2","mask_C2",target_img_size)]
+    dataloder_info = (train_val_ratio,batch_size, shuffle)
+    Dataloaders_dic = getDataloadersDic(dataset_info, dataloder_info)
+
+    dataset_info = ("/content/trainData_EndoCV2021_5_Feb2021", child_dir, imageDir, maskDir, target_img_size)
+    dataloder_info = (0.01,batch_size, shuffle) # from 0:(0.01*datasize) will be for val the rest for test
+    Dataloaders_test_dic = getDataloadersDic(dataset_info, dataloder_info)
+    Dataloaders_dic['test']=Dataloaders_test_dic['val']
+
+    print("training images:", len(Dataloaders_dic['train'].dataset))
+    print("val images:", len(Dataloaders_dic['val'].dataset))
+    print("test images:", len(Dataloaders_dic['test'].dataset))
+    print("model name:", model_name)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+    print("Training will be on:", device)
+
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    loss_fn = {'generator':nn.MSELoss(reduction='sum'), # this is generator loss,
+               'segmentor':nn.BCEWithLogitsLoss()}
+
+    # call the training loop,
+    # make sure to pass correct checkpoint path, or none if starting with the training
+    start = time.time()
+    checkpoint = torch.load('./GIANA21/checkpoints/highest_IOU_SegNetGRU_Symmetric_columns_UltimateShare.pt')
+    Dl_TOV_training_loop(num_epochs, optimizer, lamda, model, loss_fn,
+                  Dataloaders_dic, device, switch_epoch)
+
+    wandb.save(colab_dir + '/*.py')
+    wandb.save(colab_dir + '/results/*')
+    wandb.save(colab_dir + '/models/*')
+    wandb.finish()
+
+    total_time = time.time() - start
+    print('-' * 50, '\nThe entire experiments completed in {:.0f}h {:.0f}m'.format(total_time // 60 ** 2,
+                                                                                   (total_time % 60 ** 2) // 60))
--- Training.py
+++ Training.py
@@ -831,3 +831,110 @@ def show_filter(generated_masks, original_masks,kernel3D, phase, index, save):
         #     plt.show()
         #     plt.clf()
         # print(img)
+
+def Dl_TOV_inference_loop(num_epochs, optimizer, lamda, model, loss_dic, data_loader_dic, device,checkpoint):
+    best_loss = {k: 1000 for k in data_loader_dic.keys()}
+    best_iou = {k: 0 for k in data_loader_dic.keys()}
+    best_iou_epoch = -1
+    loss_fn_sum = loss_dic['generator']
+    if checkpoint:
+        model.state
+    for epoch in range(0, num_epochs + 1):
+
+        for phase in data_loader_dic.keys():
+            model.eval()  # Set model to evaluate mode
+
+            flag = True #flag for showing first batch
+            total_train_images = 0
+            # TODO: the Loss here are normalized using np.mean() to get the average loss across all images. However,
+            # TODO: last epoch may have less images hence, mean is not accurate
+            loss_batches = []
+            loss_l2_batches = []
+            loss_grad_batches = []
+            loss_mask_batches = []
+            iou_batches = []
+            original_images_grad = []
+
+            pbar = tqdm(data_loader_dic[phase], total=len(data_loader_dic[phase]))
+            for X, intermediate, original_masks in pbar:
+                batch_size = len(X)
+                total_train_images += batch_size
+
+                X = X.to(device).float()
+                intermediate = intermediate.to(device).float()  # intermediate is the mask with type of float
+                original_masks = original_masks.to(device)
+
+                generated_images = model[0](X)
+                generated_masks = model[1](generated_images)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    loss_mask = torch.zeros((1)).int()
+                    iou=0
+
+                    # loss: ‖f-g * mask(polyp)‖^2 + ‖∇g *mask(1-polyp)‖^2
+                    loss_l2 = torch.sum(torch.pow(torch.mul(generated_images - X, intermediate), 2)) / torch.sum(intermediate)
+                    gradients = color_gradient(generated_images, 'No reduction')
+                    gradients_masked = torch.mul(gradients, 1 - intermediate)  # consider only background
+                    loss_grad = torch.sum(torch.pow(gradients_masked, 2)) / torch.sum(1 - intermediate)
+                    loss = loss_grad * lamda['grad'] + loss_l2 * lamda['l2']
+                    # move to stage 3 loss: ‖f-g * mask(polyp)‖^2 + ‖∇g *mask(1-polyp)‖^2 + BCEWithLoggits
+                    bce = loss_dic['segmentor']
+                    loss_mask = bce(generated_masks, original_masks)
+                    loss = loss + loss_mask
+                    iou = IOU_class01(original_masks, generated_masks)
+
+                    loss_batches.append(loss.clone().detach().cpu().numpy())
+                    loss_l2_batches.append(loss_l2.clone().detach().cpu().numpy())
+                    loss_grad_batches.append(loss_grad.clone().detach().cpu().numpy())
+                    loss_mask_batches.append(loss_mask.clone().detach().cpu().numpy())
+                    iou_batches.append(iou)
+                    original_images_grad.append(color_gradient(X).clone().detach().cpu().numpy())
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                if flag:  # this flag
+                    flag = False
+                    max, generated_mask = generated_masks.max(dim=1)
+                    generated_mask = generated_mask.unsqueeze(dim=1)
+                    show(generated_images, X, generated_mask, phase, index=100 + epoch, save=True)
+
+
+                # update the progress bar
+                pbar.set_postfix({phase + ' Epoch': str(epoch) + "/" + str(num_epochs - 1),
+                                  'Loss': np.mean(loss_batches),
+                                  'L2': np.mean(loss_l2_batches),
+                                  'grad': np.mean(loss_grad_batches),
+                                  'BCE_loss': np.mean(loss_mask_batches),
+                                  'iou': np.mean(iou_batches),
+                                  'original_images_grad': np.mean(original_images_grad),
+                                  'best_val_loss': best_loss['val'],
+                                  'best_val_iou': best_iou['val'],
+                                  'best_test_loss': best_loss['test'],
+                                  'best_test_iou': best_iou['test'],
+                                  })
+            if phase != 'train':
+                if np.mean(loss_batches) < best_loss[phase]:
+                    print('best {} loss={} so far ...'.format(phase,np.mean(loss_batches)))
+                    wandb.run.summary["best_{}_loss_epoch".format(phase)] = epoch
+                    wandb.run.summary["best_{}_loss".format(phase)] = np.mean(loss_batches)
+                    best_loss[phase] = np.mean(loss_batches)
+                    if phase=='val':
+                        print('saving a checkpoint')
+                if np.mean(iou_batches) > best_iou[phase]:
+                    wandb.run.summary["best_{}_iou".format(phase)] = np.mean(iou_batches)
+                    wandb.run.summary["best_{}_iou_epoch".format(phase)] = epoch
+                    best_iou[phase] = np.mean(iou_batches)
+                    if phase=='val':
+                        best_iou_epoch = epoch
+                        print('best val_iou')
+                        print('testing on a test set....\n')
+
+            wandb.log({phase + "_loss": np.mean(loss_batches),
+                       phase + "_L2": np.mean(loss_l2_batches), phase + "_grad": np.mean(loss_grad_batches),
+                       phase + '_BCE_loss': np.mean(loss_mask_batches), phase + '_iou': np.mean(iou_batches),
+                       phase + '_original_images_grad': np.mean(original_images_grad), "best_val_loss": best_loss['val'],
+                       'best_val_iou': best_iou['val'], phase + "_epoch": epoch},
+                      step=epoch)
